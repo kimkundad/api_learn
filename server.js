586d57a1ea97a4d85c091abb3a805be35efc66b9
kimkundad <kim.kundad@gmail.com>
2023-09-13 11:40:59 +0700

    kim

--- server.js
+++ server.js
@@ -26,14 +26,32 @@ server.listen(port, () => {
 });
 
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: 'db-mysql-do-user-13182457-0.b.db.ondigitalocean.com',
+//   user: 'doadmin',
+//   password: 'AVNS_5-MVZDAv8ZiOuSoBAu7',
+//   database: 'learnsbuy',
+//   port: 25060,
+//   timezone: '+07:00'
+// });
+
+const connection = mysql
+ .createPool({
   host: 'db-mysql-do-user-13182457-0.b.db.ondigitalocean.com',
   user: 'doadmin',
   password: 'AVNS_5-MVZDAv8ZiOuSoBAu7',
   database: 'learnsbuy',
   port: 25060,
-  timezone: '+07:00'
-});
+  timezone: '+07:00',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+ })
+  .promise();
 
 connection.connect((err) => {
   if (err) {
